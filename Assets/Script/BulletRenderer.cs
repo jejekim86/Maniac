869fbbcdf79e264c254439a05add3a7df9a68b52
LeeEunSeok <a4121240@naver.com>
2024-06-04 23:17:14 +0900

    KAB - 119 GPU INSTANCING 구현

--- Assets/Script/BulletRenderer.cs
+++ Assets/Script/BulletRenderer.cs
@@ -1,33 +1,41 @@
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEditor;
 using UnityEngine;
 
 public class BulletRenderer : MonoBehaviour
 {
     [SerializeField] private Mesh bulletMeshHead;
-    [SerializeField] private Material bulletMaterialHead;
+    [SerializeField] private Material bulletMaterial;
     [SerializeField] private Mesh bulletMeshBody;
-    [SerializeField] private Material bulletMaterialBody;
 
-    float z = -0.025456795f;
+    readonly float z = -0.025456795f;
 
-    public void RenderBulletsAtPositions(List<Vector3> positions, List<Quaternion> rotations)
+    static public BulletRenderer Instance { get; private set; }
+
+    private void Start()
     {
+        if(Instance == null)
+            Instance = this;
+    }
 
-
+    public void RenderBulletsAtPositions(NativeArray<Vector3> positions, NativeArray<Quaternion> rotations)
+    {
         // Create instance data array
-        Matrix4x4[] instanceData = new Matrix4x4[positions.Count];
-        for (int i = 0; i < positions.Count; i++)
+        Matrix4x4[] instanceDataHead = new Matrix4x4[positions.Length];
+        Matrix4x4[] instanceDataBody = new Matrix4x4[positions.Length];
+
+        for (int i = 0; i < positions.Length; i++)
         {
             // Create a transformation matrix from the position
-            Matrix4x4 matrix = Matrix4x4.TRS(positions[i] + new Vector3(0, 0, z), rotations[i], Vector3.one);
-            instanceData[i] = matrix;
+            Matrix4x4 matrix = Matrix4x4.TRS(positions[i] + new Vector3(0, 0, z), rotations[i] * Quaternion.Euler(0, 90, 0), Vector3.one);
+            instanceDataHead[i] = matrix;
+            matrix = Matrix4x4.TRS(positions[i], rotations[i] * Quaternion.Euler(0,90,0), Vector3.one);
+            instanceDataBody[i] = matrix;
         }
 
         // Render using Graphics.DrawMeshInstanced
-        Graphics.DrawMeshInstanced(bulletMeshHead, 0, bulletMaterialHead, instanceData, positions.Count);
-        Graphics.DrawMeshInstanced(bulletMeshBody, 0, bulletMaterialBody, instanceData, positions.Count);
-
+        Graphics.DrawMeshInstanced(bulletMeshHead, 0, bulletMaterial, instanceDataHead, positions.Length);
+        Graphics.DrawMeshInstanced(bulletMeshBody, 0, bulletMaterial, instanceDataBody, positions.Length);
     }
-
 }
--- Assets/Script/Weapon/LongRangeWeaponShotgun.cs
+++ Assets/Script/Weapon/LongRangeWeaponShotgun.cs
@@ -9,8 +9,7 @@ public class LongRangeWeaponShotgun : LongRangeWeapon
 
     NativeArray<Vector3> bulletsTransform;
     NativeArray<Vector3> bulletsTargetTransform;
-
-    bool finishShoot = false;
+    NativeArray<Quaternion> rotation;
 
     public override void SetData()
     {
@@ -26,7 +25,7 @@ public class LongRangeWeaponShotgun : LongRangeWeapon
     {
         base.Update();
     }
-    Bullet newBullet;
+ 
     public override bool Attack()
     {
         if (timeCount < reloadT)
@@ -34,31 +33,6 @@ public class LongRangeWeaponShotgun : LongRangeWeapon
             return false;
         }
 
-        /*
-        Quaternion startAngle = fireTr.rotation * Quaternion.Euler(0, -15, 0);
-        Quaternion endAngle = fireTr.rotation * Quaternion.Euler(0, 15, 0);
-
-        ShootBulletJob shootBulletJob;
-
-        var rotation = new NativeArray<Quaternion>(numberBulletsFire, Allocator.TempJob);
-        shootBulletJob.startAngle = startAngle;
-        shootBulletJob.endAngle = endAngle;
-        shootBulletJob.numberBulletsFire = numberBulletsFire;
-        shootBulletJob.rot = rotation;
-
-        JobHandle jobHandle = shootBulletJob.Schedule(numberBulletsFire, numberBulletsFire / 3);
-
-        jobHandle.Complete();
-
-        Bullet[] bullets;
-        PoolManager.instance.bulletPool.GetObjects(out bullets, numberBulletsFire);
-        for (int i = 0; i < bullets.Length; i++)
-        {
-            bullets[i].backInPool += () => PoolManager.instance.bulletPool.PutInPool(bullets[i]);
-            bullets[i].transform.position = fireTr.position;
-            bullets[i].transform.rotation = rotation[i];
-        }
-        */
         StartCoroutine(CoShootBullet());
         
         timeCount = 0;
@@ -74,8 +48,6 @@ public class LongRangeWeaponShotgun : LongRangeWeapon
         public NativeArray<Quaternion> rot;
         public void Execute(int index)
         {
-            //Quaternion rotation = Quaternion.Lerp(startAngle, endAngle, (float)1 / numberBulletsFire * index);
-            //rot[index] = rotation;
             rot[index] = Quaternion.Lerp(startAngle, endAngle, (float)1 / numberBulletsFire * index);
         }
     }
@@ -89,7 +61,7 @@ public class LongRangeWeaponShotgun : LongRangeWeapon
 
         ShootBulletJob shootBulletJob;
 
-        var rotation = new NativeArray<Quaternion>(numberBulletsFire, Allocator.TempJob);
+        rotation = new NativeArray<Quaternion>(numberBulletsFire, Allocator.TempJob);
         shootBulletJob.startAngle = startAngle;
         shootBulletJob.endAngle = endAngle;
         shootBulletJob.numberBulletsFire = numberBulletsFire;
@@ -105,7 +77,6 @@ public class LongRangeWeaponShotgun : LongRangeWeapon
         PoolManager.instance.bulletPool.GetObjects(out bullets, numberBulletsFire);
         for (int i = 0; i < numberBulletsFire; i++)
         {
-            //bullets[i].backInPool += () => PoolManager.instance.bulletPool.PutInPool(bullets[i]);
             bullets[i].transform.position = fireTr.position;
             bullets[i].transform.rotation = rotation[i];
 
@@ -144,13 +115,14 @@ public class LongRangeWeaponShotgun : LongRangeWeapon
                 bullets[i].transform.position = lerpTransform[i];
             }
 
+            BulletRenderer.Instance.RenderBulletsAtPositions(lerpTransform, rotation);
+
             yield return null;
         }
         for(int i = 0; i < numberBulletsFire; i++)
         {
             PoolManager.instance.bulletPool.PutInPool(bullets[i]);
         }
-        finishShoot = false;
     }
 
     struct MoveBulletJob : IJobParallelFor
