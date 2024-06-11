2a2b25e3eb56624758f421692e5c24687e6398ce
shirping1 <a4121240@naver.com>
2024-06-11 15:05:37 +0900

    skill 정보 불러오기

--- Assets/Script/DB/DBTest.cs
+++ Assets/Script/DB/DBTest.cs
@@ -2,12 +2,14 @@ using MySql.Data.MySqlClient;
 using System.Data;
 using System;
 using UnityEngine;
+using System.Data.SqlClient;
+using static UnityEngine.GraphicsBuffer;
 
 public class DBTest : MonoBehaviour
 {
     static string ipAddress = "localhost";
     static string db_id = "root";
-    static string db_pw = "1234";
+    static string db_pw = "";
     static string db_name = "mydb";
     static string strConn = new MySqlConnectionStringBuilder
     {
@@ -18,6 +20,8 @@ public class DBTest : MonoBehaviour
         CharacterSet = "utf8",
         SslMode = MySqlSslMode.None
     }.ToString();
+
+    static string selectAllSkillData = "Select * from skill";
 
     public static MySqlConnection SqlConn;
 
@@ -36,22 +40,60 @@ public class DBTest : MonoBehaviour
 
     void Start()
     {
+        /*
         if (SqlConn == null)
         {
             Debug.LogError("Start 메서드에서 SqlConn이 null입니다.");
             return;
         }
 
-        string sql = "SELECT * FROM user;";
-        DataSet ds = OnSelectRequest(sql, "user");
+        //string sql = "SELECT * FROM user;";
+        string sql = "Select * from skill";
+        DataSet ds = OnSelectRequest(sql, "skill");
 
         if (ds != null)
         {
             Debug.Log(ds.GetXml());
+            //Debug.Log(ds.)
         }
         else
         {
             Debug.LogError("데이터베이스에서 데이터를 가져오는 데 실패했습니다.");
+        }
+        */
+        DataSet ds = GetSkillData();
+        //string sjon = JsonConvert.SerializeXmlNode(ds.GetXml());
+        Debug.Log(ds.GetXml());
+    }
+
+    public static DataSet GetSkillData()
+    {
+        if (SqlConn == null)
+        {
+            Debug.LogError("OnSelectRequest 메서드에서 SqlConn이 null입니다.");
+            return null;
+        }
+
+        try
+        {
+            SqlConn.Open();   //DB 연결
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = SqlConn;
+            cmd.CommandText = selectAllSkillData;
+
+            MySqlDataAdapter sd = new MySqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sd.Fill(ds, "Skill");
+
+            SqlConn.Close();  //DB 연결 해제
+
+            return ds;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("데이터베이스 작업 실패: " + e.ToString());
+            return null;
         }
     }
 
@@ -70,6 +112,15 @@ public class DBTest : MonoBehaviour
             MySqlCommand cmd = new MySqlCommand();
             cmd.Connection = SqlConn;
             cmd.CommandText = p_query;
+            
+            /*
+            MySqlDataReader reader = cmd.ExecuteReader();
+            
+            while (reader.Read())
+            {
+                Debug.Log(reader.GetInt32(0) + "\n" + reader.GetString(1));
+            }
+            */
 
             MySqlDataAdapter sd = new MySqlDataAdapter(cmd);
             DataSet ds = new DataSet();
