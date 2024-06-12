2d51b3a16454d67d0621d3f5214a45a4bc40edcb
shirping1 <a4121240@naver.com>
2024-06-12 17:43:35 +0900

    KAN-84 사운드 매니저 기초 구현

--- Assets/Script/SoundManager.cs
+++ Assets/Script/SoundManager.cs
@@ -2,18 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SoundEffect
+{
+    fireBullet,
+    explosion
+}
+
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get; private set; }
+
+    [SerializeField] private AudioSource bgmAudioSource;
+    [SerializeField] private AudioSource effectAudioSource;
+
+    [SerializeField] private AudioClip[] audioClips;
 
     void Start()
     {
         if(Instance == null)
             Instance = this;
     }
-
-    void Update()
+    
+    public void PlaySoundEffect(SoundEffect soundEffect)
     {
-        
+        effectAudioSource.PlayOneShot(audioClips[(int)soundEffect]);
     }
 }
