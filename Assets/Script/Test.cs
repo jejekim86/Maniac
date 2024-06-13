5eba65c7a2259a22591a9248e655e72a971c275b
jejekim86 <jejekim8672@gmail.com>
2024-06-13 11:12:22 +0900

    Revert "Merge pull request #19 from jejekim86/RideUI"
    This reverts commit 30222c5a36686a23019abff1ccc2031e6f7b39b2, reversing
    changes made to 0d10eaec3af39e295c6c7c4999a708a5bb6c9d98.

--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -1,121 +1,48 @@
-﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using Unity.VisualScripting;
-using UnityEditor.Rendering;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Test : Controller
+/*public class Test : TestController
 {
-    Vector3 translation;
+    MeshRenderer meshRenderer;
     CapsuleCollider collider;
-    [SerializeField] float movespeed;
-    [SerializeField] Weapon longRangeWeapon;
+    Vector3 translation;
+
+
     private void Start()
     {
-        collider = GetComponent<CapsuleCollider>();
-        meshRenderer = GetComponent<MeshRenderer>();
-        rigidbody = GetComponent<Rigidbody>();
-        //rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
         isride = false;
-        HP_image = null;
-        vehicle = null;
+        meshRenderer = GetComponent<MeshRenderer>();
+        collider = GetComponent<CapsuleCollider>();
     }
+
+    private void Update()
+    {
+        switch (vehicle)
+        {
+            case null:
+                Move();
+                break;
+            default:
+                vehicle.Move();
+                Interact();
+                break;
+        }
+    }
+
+    
 
     public override void Move()
     {
         float horizontalMove = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        float speed = movespeed;
-
-        translation = new Vector3(horizontalMove, 0, vertical);
-        translation *= speed * Time.fixedDeltaTime;
-        //translation *= speed * Time.deltaTime;
-        rigidbody.MovePosition(rigidbody.position + translation);
-
+        translation = Vector3.forward * (vertical * Time.deltaTime);
+        translation += Vector3.right * (horizontalMove * Time.deltaTime);
+        translation *= 10;
+        transform.Translate(translation, Space.World);
     }
 
+   
 
-    IEnumerator ClickButton(Vehicle item = null)
-    {
-        switch (isride)
-        {
-            case true: // 차에서 내릴때
-                yield return null;
-                transform.SetParent(null);
-                transform.position = vehicle.transform.position + (Vector3.right * 3);
-                isride = false;
-                vehicle = null;
-                SetColliderEnabled(true);
-                break;
-            case false:
-                if (!item) yield break; // 차에서 탑승할때
-                Debug.Log("ClickButton");
-                yield return new WaitForSeconds(3f);
-                isride = true;
-                vehicle = item;
-                transform.SetParent(vehicle.gameObject.transform);
-                SetColliderEnabled(false);
-                break;
-        }
-        yield break;
-    }
-
-
-    private void OnCollisionStay(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Vehicle"))
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                StartCoroutine(ClickButton(collision.gameObject.GetComponent<Vehicle>()));
-            }
-        }
-    }
-
-
-
-    private void FixedUpdate()
-    {
-        switch (vehicle == null)
-        {
-            case false:
-                vehicle.Move();
-                transform.localPosition = Vector3.zero;
-                transform.localRotation = Quaternion.Euler(Vector3.zero);
-                if (Input.GetKeyDown(KeyCode.E))
-                    StartCoroutine(ClickButton());
-                break;
-            case true:
-                Move();
-                break;
-        }
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            if (longRangeWeapon) return;
-            if (longRangeWeapon.Attack())
-                Debug.Log("Attack");
-
-
-            Debug.Log("Input.GetMouseButton(0)");
-        }
-    }
-
-    public void TestSetLongRangeWeapon(Weapon weapon)
-    {
-        longRangeWeapon = weapon;
-    }
-
-    public void SetColliderEnabled(bool check)
-    {
-        collider.isTrigger = !check;
-        meshRenderer.enabled = check;
-    }
-}
+}*/
--- Assets/Script/Vehicle.cs
+++ Assets/Script/Vehicle.cs
@@ -1,43 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
 public class Vehicle : Controller
 {
     [SerializeField] WheelCollider[] wheelColliders;
-    Controller rideObject;
+
     public override void AddHp(float heal) => base.AddHp(heal);
     public override void GetDamage(float damage) => base.GetDamage(damage);
     private void Start()
     {
         maxHp = 100;
-        isride = false;
     }
-
-    //public void CheckRide(bool check) => isride = check;
-
-    //private void Update()
-    //{
-    //    if (isride)
-    //        Move();
-    //}
-
     public override void Move()
     {
         wheelColliders[0].steerAngle = wheelColliders[1].steerAngle = Input.GetAxis("Horizontal") * 20f;
-        for (int i = 0; i < wheelColliders.Length; i++)
-        {
-            float torque = -1 * (Input.GetAxis("Vertical") * 20000f);
-            if (torque != 0)
-            {
-                wheelColliders[i].motorTorque = torque;
-                Debug.Log("motorTorque : " + wheelColliders[i].motorTorque);
-            }
-            else
-                wheelColliders[i].brakeTorque = torque;
-        }
-
+        for(int i = 0; i <  wheelColliders.Length; i++)
+            wheelColliders[i].motorTorque = -1 * Input.GetAxis("Vertical") * (2000f * 0.25f);
     }
+
+
+
 }
