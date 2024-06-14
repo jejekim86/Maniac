9012e465e6283d8509f478aa0a5153d4f574f378
jejekim86 <jejekim8672@gmail.com>
2024-06-14 13:27:26 +0900

    Vehicle Class
    vehicle Class move

--- Assets/Script/Vehicle.cs
+++ Assets/Script/Vehicle.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using UnityEngine;
 
 public class Vehicle : Controller
@@ -18,9 +19,23 @@ public class Vehicle : Controller
         wheelColliders[0].steerAngle = wheelColliders[1].steerAngle = Input.GetAxis("Horizontal") * 20f;
         for (int i = 0; i < wheelColliders.Length; i++)
         {
-            wheelColliders[i].motorTorque = -1 * (Input.GetAxis("Vertical") * 10000f);
-            Debug.Log("motorTorque : " + wheelColliders[i].motorTorque);
+            wheelColliders[i].motorTorque = -1 * (Input.GetAxis("Vertical") * 4000f);
+            Debug.Log("motorTorque : " + wheelColliders[0].motorTorque);
         }
 
+        if (Input.GetKeyDown(KeyCode.Space))
+            ApplyBrakeTorque(50000);
+
+        else
+            ApplyBrakeTorque(0);
+    }
+
+    public void ApplyBrakeTorque(float value)
+    {
+        for (int i = 0; i < wheelColliders.Length; i++)
+        {
+            wheelColliders[i].brakeTorque = value;
+            Debug.Log("BrakeTorque");
+        }
     }
 }
