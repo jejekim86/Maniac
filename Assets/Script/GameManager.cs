682a6be49b1b49812be78f8d4fb84558ef2b39bd
LeeEunSeok <a4121240@naver.com>
2024-06-16 01:35:04 +0900

    인게임 업그레이드 작업
    이동속도, 총알 공격력, 돈 획등량 증가 연결
    차량속도, 차량 훔치는 속도, 체력 회복 미연결

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -10,10 +10,26 @@ public sealed class GameManager : MonoBehaviour
 {
     //[SerializeField] UIManager Uimanager;
     //[SerializeField] Datamanager Datamanager;
+    public enum Upgrade
+    {
+        gameTime, moveSpeed, money, maxHealth,
+        vehicleMaxSpeed, vehicleRobSpeed, damage, max
+    }
+    public enum InGameUpgrade
+    {
+        moveSpeed, money, vehicleMaxSpeed,
+        vehicleRobSpeed, damage, healthRecovery, max
+    }
+
+    public float[] upgradeData1 = new float[(int)Upgrade.max];
+    public float[] UpgradeData1 { get{ return upgradeData1; }}
+
+    public float[] inGameUpgradeData1 = new float[(int)Upgrade.max];
+    public float[] InGameUpgradeData1 { get { return inGameUpgradeData1; } }
 
     public struct UpgradeData
     {
-        public float gameTime {get; private set;}
+        public float gameTime { get; private set; }
         public float moveSpeed { get; private set; }
         public int money { get; private set; }
         public float magnetDistance { get; private set; }
