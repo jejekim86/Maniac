b788978c3fe53e6713af10b0a5b48bd77bcd37d8
YeonJangL <lej32552002@gmail.com>
2024-06-18 19:30:19 +0900

    자동차 태그 수정

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -1,10 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using Unity.Burst.CompilerServices;
-using UnityEditorInternal;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class Player : Controller
@@ -41,6 +37,7 @@ public class Player : Controller
 
     public override void AddHp(float heal) => base.AddHp(heal);
     public override void GetDamage(float damage) => base.GetDamage(damage);
+
     private void Start()
     {
         coolTime_Bag.gameObject.SetActive(false);
@@ -88,15 +85,7 @@ public class Player : Controller
         dashTarget = transform.position + dashDirection.normalized * dashPower;
         Vector3 startPos = transform.position;
         rigidbody.AddForce(dashDirection.normalized * dashPower, ForceMode.Impulse);
-        /*
-        while (elapsed < dashDuration) 
-        {
-            rigidbody.MovePosition(Vector3.Lerp(startPos, dashTarget, elapsed / dashDuration)); // ���� ��ġ�� ��ǥ ��ġ�� ���� ����
-            //transform.position = Vector3.Lerp(startPos, dashTarget, elapsed / dashDuration); // ���� ��ġ�� ��ǥ ��ġ�� ���� ����
-            elapsed += Time.deltaTime; 
-            yield return new WaitForFixedUpdate(); 
-        }
-        */
+
         yield return new WaitForSeconds(0.5f);
         rigidbody.velocity = Vector3.zero;
         isDashing = false;
@@ -118,8 +107,6 @@ public class Player : Controller
         }
 
         translation *= speed * Time.fixedDeltaTime;
-        //translation *= speed * Time.deltaTime;
-        //transform.Translate(translation, Space.World);
         rigidbody.MovePosition(rigidbody.position + translation);
 
         if (Input.GetKey(KeyCode.Space) && canDash && !isDashing)
@@ -132,31 +119,6 @@ public class Player : Controller
         animator.SetFloat("Vertical", vertical, 0.1f, Time.deltaTime);
         animator.SetFloat("Horizontal", horizontalMove, 0.1f, Time.deltaTime);
         animator.SetFloat("WalkSpeed", animSpeed);
-
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //RaycastHit hit;
-        //if (Physics.Raycast(ray, out hit))
-        //    transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
-        //
-        //if (Input.GetMouseButton(0))
-        //{
-        //    if (longRangeWeapon == null) return;
-        //    if (longRangeWeapon.Attack())
-        //    {
-        //        StartCoroutine(UpdateCooldownSlider(coolTime_Gun, longRangeWeapon.GetReloadTime()));
-        //        coolTime_Gun.value = 1;
-        //    }
-        //}
-        //if (Input.GetMouseButton(1))
-        //{
-        //    if (meleeWeapon.Attack())
-        //    {
-        //        animator.SetTrigger("MeleeAttack");
-        //        StartCoroutine(UpdateCooldownSlider(coolTime_Bag, meleeWeapon.GetReloadTime()));
-        //        coolTime_Bag.value = 1;
-        //    }
-        //}
-
     }
 
     public void Attack()
@@ -204,7 +166,6 @@ public class Player : Controller
         canDash = true;
     }
 
-
     private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.CompareTag("Vehicle"))
@@ -215,13 +176,11 @@ public class Player : Controller
     {
         rigidbody.constraints = RigidbodyConstraints.FreezeAll;
     }
-    
+
     private void OnCollisionExit(Collision collision)
     {
         rigidbody.constraints = RigidbodyConstraints.None;
     }
-
-
 
     private void FixedUpdate()
     {
@@ -247,13 +206,11 @@ public class Player : Controller
 
     private void AttractItems()
     {
-
         GameObject[] items = GameObject.FindGameObjectsWithTag("Item");
 
         foreach (GameObject item in items)
         {
             Vector3 relativePos = item.transform.position - transform.position;
-
 
             if (relativePos.magnitude <= itemRange)
                 item.transform.position = Vector3.Lerp(item.transform.position, transform.position, itemMoveSpeed * Time.deltaTime);
@@ -274,6 +231,7 @@ public class Player : Controller
                 break;
         }
     }
+
     IEnumerator IncreaseSlider(Vehicle item)
     {
         float duration = 3.0f; // 슬라이더가 채워지는 시간
@@ -284,7 +242,7 @@ public class Player : Controller
             coolTime_Ride.transform.position = Camera.main.WorldToScreenPoint(item.transform.position);
             coolTime_Ride.value += (coolTime_Ride.maxValue / duration) * Time.deltaTime;
             coolTime_Ride.value = Mathf.Clamp(coolTime_Ride.value, 0, coolTime_Ride.maxValue);
-            yield return null; // 다음 프레임까지 대기
+            yield return null;
         }
 
         coolTime_Ride.value = coolTime_Ride.maxValue;
@@ -292,6 +250,7 @@ public class Player : Controller
         coolTime_Ride.value = 0;
         coolTime_Ride.gameObject.SetActive(false);
     }
+
     IEnumerator ClickButton(Vehicle item = null)
     {
         switch (vehicle)
@@ -305,6 +264,7 @@ public class Player : Controller
                 transform.localPosition = Vector3.zero;
                 transform.localRotation = Quaternion.Euler(Vector3.zero);
                 rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+                vehicle.gameObject.tag = "Ride";
                 SetColliderEnabled(false);
                 break;
             default: // 차에서 내릴때
@@ -312,12 +272,14 @@ public class Player : Controller
                 transform.position = vehicle.transform.position + (Vector3.right * 3);
                 rigidbody.constraints = RigidbodyConstraints.None;
                 vehicle.SetHp_imageActive(false);
+                vehicle.gameObject.tag = "Vehicle";
                 vehicle = null;
                 SetColliderEnabled(true);
                 break;
         }
         yield break;
     }
+
     public void SetColliderEnabled(bool check)
     {
         collider.enabled = check;
